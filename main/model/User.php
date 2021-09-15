903075492d172951e531f5299248bac89eecb3a7
Nick <nick.vakuloff@gmail.com>
2021-09-14 20:54:58 -0400

    Added recent active users to index page

--- main/model/User.php
+++ main/model/User.php
@@ -12,5 +12,20 @@ class User
         $user = $pst->fetch(PDO::FETCH_OBJ);
         return $user;
 	}
+
+	public function getRecentActiveUsers($dbcon)
+	{
+		$sql = "SELECT UsersxMusic.id, UsersxMusic.user_id, Users.first_name, 
+				Users.last_name, Users.img, Max(UsersxMusic.date) AS max_date
+				FROM `UsersxMusic` 
+				INNER JOIN Users ON UsersxMusic.user_id = Users.id
+				GROUP BY UsersxMusic.user_id 
+				ORDER BY max_date DESC LIMIT 10";
+		$pst = $dbcon->prepare($sql); 
+  		$pst->execute();
+		
+		$music = $pst->fetchAll(PDO::FETCH_OBJ);
+		return $music;
+	}
 }
 ?>
--- main/views/index.php
+++ main/views/index.php
@@ -4,6 +4,9 @@
 	$dbcon = Database::getDb();
 	$m = new Music();
 	$rating = $m->getTopSongs($dbcon);
+
+	$u = new User();
+	$recent_active_users = $u->getRecentActiveUsers($dbcon);
 ?>
 <div class="container">
 	<div class="row g-0">
@@ -19,6 +22,21 @@
 					</form>
 				</div>
 				<!-- Content -->
+				<div class="userActive">
+					<div class="userActive_title">
+						<h3>Последние активные пользователи</h3>
+						<!-- <a target="_blank" href="/access-is-denied.html">Посмотреть всех</a> -->
+					</div>
+					<div class="userActive_list">
+						<? foreach($recent_active_users as $recent_active_users): ?>
+							<a href="profile.php?user_id=<?echo $recent_active_users->user_id?>" class="userActive_item">
+								<div class="userActive_photo">
+									<img src="../images/dist/main/avatars/<?php echo $recent_active_users->img ?>" alt="UserName 1">
+								</div>
+							</a>
+						<? endforeach; ?>
+					</div>
+				</div>
 				<div class="topTracks">
 					<h3>Топ треки</h3>
 					<div class="container">
@@ -28,30 +46,30 @@
 									<!-- ----------------------------- -->
 									<? foreach($rating as $rating): ?>
 										<div class="topTracks_item">
-										<audio class="player" controls>
-									  		<source src="<? echo $rating->link ?>" type="audio/mp3" />
-										</audio>
-										<div class="topTracks_info">
-											<h6><? echo $rating->author ?></h6>
-											<p><? echo $rating->title ?></p>
-										</div>
-										<div class="topTracks_wrapper">
-											<div class="topTracks_likes disabled_btn">
-												<? echo $rating->likes_count ?> 
-												<span class="like"></span>
+											<audio class="player" controls>
+									  			<source src="<? echo $rating->link ?>" type="audio/mp3" />
+											</audio>
+											<div class="topTracks_info">
+												<h6><? echo $rating->author ?></h6>
+												<p><? echo $rating->title ?></p>
 											</div>
-											<div class="topTracks_user">
-												<div class="userInfo">
-													Загружен
-													<span><? echo $rating->first_name . ' ' . $rating->last_name ?></span>
+											<div class="topTracks_wrapper">
+												<div class="topTracks_likes disabled_btn">
+													<? echo $rating->likes_count ?> 
+													<span class="like"></span>
 												</div>
-												<a href="profile.php?user_id=<? echo $rating->user_id ?>" class="userActive_item">
-													<div class="userActive_photo">
-														<img src="../images/dist/main/avatars/<?php echo $rating->img ?>" alt="UserName 1">
+												<div class="topTracks_user">
+													<div class="userInfo">
+														Загружен
+														<span><? echo $rating->first_name . ' ' . $rating->last_name ?></span>
 													</div>
-												</a>
+													<a href="profile.php?user_id=<? echo $rating->user_id ?>" class="userActive_item">
+														<div class="userActive_photo">
+															<img src="../images/dist/main/avatars/<?php echo $rating->img ?>" alt="UserName 1">
+														</div>
+													</a>
+												</div>
 											</div>
-										</div>
 										</div>
 									<? endforeach; ?>
 									<!-- -------------------------------- -->
